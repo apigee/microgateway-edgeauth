39e8c08995c6f0cc08ffb1d70c7865840433926c
Nandan Sridhar <srinandans@google.com>
2019-01-04 14:39:50 -0800

    add pragma header

--- apiproxy/resources/jsc/set-response.js
+++ apiproxy/resources/jsc/set-response.js
@@ -27,4 +27,5 @@
  //send response
  context.setVariable("response.header.Content-Type","application/json");
  context.setVariable("response.header.Cache-Control","no-store");
+ context.setVariable("response.header.Pragma","no-cache");
  context.setVariable("response.content", JSON.stringify(jws));
