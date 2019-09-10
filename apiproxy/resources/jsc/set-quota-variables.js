5f277fcd446bbdcc67c23a303f8ca5e1e5e1b015
Keyur Karnik <keyur.karnik+github@gmail.com>
2019-09-10 02:26:13 -0700

    140090623 Remove org name from quota identifier

--- apiproxy/resources/jsc/set-quota-variables.js
+++ apiproxy/resources/jsc/set-quota-variables.js
@@ -1,7 +1,5 @@
  
-var orgName = context.getVariable('apigee.edgemicro.organization');
-
-context.setVariable('quota.identifier', orgName + '.' + request.body.asJSON.identifier);
+context.setVariable('quota.identifier', request.body.asJSON.identifier);
 context.setVariable("quota.allow",request.body.asJSON.allow);
 context.setVariable("quota.interval",request.body.asJSON.interval);
 context.setVariable("quota.unit",request.body.asJSON.timeUnit);
