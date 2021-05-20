84bb3593a56f59094c6aea5fee65d2ca1682da96
Sumit Parakh <sumit.thecoder@gmail.com>
2021-05-20 07:42:23 +0000

    185323500 parse expires_in into integer

--- apiproxy/resources/jsc/set-response.js
+++ apiproxy/resources/jsc/set-response.js
@@ -26,12 +26,14 @@
     jws.access_token = context.getVariable('jwtmessage');
     jws.token_type   = "Bearer";
 
-     // for /token flow
-     jws.expires_in = context.getVariable("oauthv2accesstoken.AccessTokenRequest.expires_in");
+    // for /token flow
+    jws.expires_in = context.getVariable("oauthv2accesstoken.AccessTokenRequest.expires_in");
 
      // for any other flows if any 
     if ( !jws.expires_in ) {
       jws.expires_in   = parseInt( context.getVariable("token_expiry"), 10) / 1000; // convert to seconds
+    } else {
+       jws.expires_in = parseInt(jws.expires_in);
     }
     
     //if refresh token exists, add it to response
